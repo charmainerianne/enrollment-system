0f3d580fff3653bb4505873eb12cb9802abb1cf2
Charmaine Rianne Q. Bordario <158406413+charmainerianne@users.noreply.github.com>
2025-05-28 00:08:11 +0800

    Add Controllers/HomeController.cs 1748362121877

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnrollmentSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
