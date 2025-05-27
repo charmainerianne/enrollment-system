759d6df58043abfc13b7695523ab1a8883bd4ef2
Charmaine Rianne Q. Bordario <158406413+charmainerianne@users.noreply.github.com>
2025-05-28 01:37:11 +0800

    Add Data/EnrollmentContext.cs 1748367462226

--- Data/EnrollmentContext.cs
+++ Data/EnrollmentContext.cs
@@ -1,34 +1,30 @@
-using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
 using EnrollmentSystem.Models;
 
 namespace EnrollmentSystem.Data
 {
     public class EnrollmentContext : DbContext
     {
-        public EnrollmentContext(DbContextOptions<EnrollmentContext> options) : base(options)
+        public EnrollmentContext() : base("name=EnrollmentContext")
         {
         }
 
         public DbSet<Student> Students { get; set; }
-        public DbSet<Admin> Admins { get; set; }
         public DbSet<Course> Courses { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Schedule> Schedules { get; set; }
         public DbSet<SemesterYear> SemesterYears { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<TeachAssignment> TeachAssignments { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<StudentHistory> StudentHistories { get; set; }
+        public DbSet<AdminHistory> AdminHistories { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+            // Fluent API configurations if needed
             base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<Enrollment>()
-                .HasOne(e => e.Student)
-                .WithMany(s => s.Enrollments)
-                .HasForeignKey(e => e.StudentId);
-
-            modelBuilder.Entity<Enrollment>()
-                .HasOne(e => e.Course)
-                .WithMany(c => c.Enrollments)
-                .HasForeignKey(e => e.CourseId);
         }
     }
 }
